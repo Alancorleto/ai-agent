c1c9064194d030433b80cc020af017d963411a98
alancorleto <corletoalan@gmail.com>
2025-07-29 13:46:18 -0300

    Add --verbose argument

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print('Usage: python main.py "prompt"')
         sys.exit(1)
         return
+
+    verbose = False
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        verbose = True
 
     load_dotenv()
     my_api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,14 +19,25 @@ def main():
     client = genai.Client(api_key=my_api_key)
 
     prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt
+        contents=messages
     )
 
+    if verbose:
+        print(f"User prompt: {prompt}")
+    
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
