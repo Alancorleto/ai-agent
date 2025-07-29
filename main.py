b68878ecf53707c267f46ec6f5ff753797cf7d1b
alancorleto <corletoalan@gmail.com>
2025-07-29 13:37:42 -0300

    Accept any prompt as an argument.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-my_api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=my_api_key)
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: python main.py "prompt"')
+        sys.exit(1)
+        return
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+    load_dotenv()
+    my_api_key = os.environ.get("GEMINI_API_KEY")
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    client = genai.Client(api_key=my_api_key)
+
+    prompt = sys.argv[1]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=prompt
+    )
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
